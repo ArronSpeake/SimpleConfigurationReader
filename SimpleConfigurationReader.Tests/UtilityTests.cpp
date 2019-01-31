acf64d51e710f1bdb23a36f83ff3fd5e2f4813b3
Arron Speake <speake.arron@gmail.com>
2019-01-31 15:21:05 +0000

    Passed Can_Replace_Single_Chars Utility test

--- SimpleConfigurationReader.Tests/UtilityTests.cpp
+++ SimpleConfigurationReader.Tests/UtilityTests.cpp
@@ -5,11 +5,15 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 #include "Utility.h"
 
+#include <string>
+
 namespace UtilityTests {
   TEST_CLASS(Utility_Tests) {
 public:
     TEST_METHOD(Can_Replace_Single_Chars) {
-
+      std::string string = "abcd";
+      Utility::replace(string, "a", "e");
+      Assert::AreEqual("ebcd", string.c_str());
     }
   };
 }
--- SimpleConfigurationReader/Utility.cpp
+++ SimpleConfigurationReader/Utility.cpp
@@ -1,5 +1,14 @@
 #include "Utility.h"
 
 void Utility::replace(std::string &string, const std::string &substr, const std::string &replace) {
-  
+  size_t i = 0;
+  while (true) {
+    i = string.find(substr, i); // Find substring at or after i-th char.
+    if (i == std::string::npos) break;
+
+    string.erase(i, substr.length());
+    string.insert(i, replace);
+
+    i += replace.length();
+  }
 }
